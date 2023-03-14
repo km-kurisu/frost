07d5d460740cad6d90a10c76a7037767a193ed1c
km-kurisu <kamleshkmistry33@gmail.com>
2023-03-14 12:28:16 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container,Paper,Typography, containerClasses } from '@mui/material'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container 
+    sx={{ height:"100vh"  , textAlign:'center' , py:20 , color:'purple'}} >
+      <Typography 
+      variant='h2'
+      fontFamily={'monospace'}>
+        Portfolio V.1
+      </Typography>
+      <Button variant='normal'
+      sx={{my:10 , px:10 , fontSize:20 , color:'navy' }}>
+        GitHub
+      </Button>
+      <Paper  elevation={8} 
+      sx = {{py:5 , my:3 , fontFamily:'monospace' , fontSize:'20'}}
+      >this is portfolio v.1</Paper>
+    </Container>
+
   );
 }
 
